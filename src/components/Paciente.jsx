7840913536e8191e4a0c5ca727c36abba0d2ec50
francocoll <91277981+francocoll@users.noreply.github.com>
2022-05-04 23:27:47 -0300

    nuevo form

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,5 +1,5 @@
 const Paciente = ({ paciente, setPaciente,eliminarPaciente }) => {
-    const { nombre, email, alta, sintomas, id } = paciente
+    const { nombre, propietario, email, alta, sintomas, id } = paciente
     const handleEliminar = () => {
         const respuesta = confirm('Deseas eliminar?')
         if (respuesta){
@@ -8,8 +8,11 @@ const Paciente = ({ paciente, setPaciente,eliminarPaciente }) => {
     }
     return (
         <div className="m-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
-            <p className="font-bold mb-3 text-gray-700 uppercase"> Propietario:{''}
-                <span className="font-normal normal-case">{nombre}</span>
+            <p className="font-bold mb-3 text-gray-700 uppercase"> Nombre: {''}
+                <span className="font-normal normal-case">{ nombre}</span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase"> Propietario: {''}
+                <span className="font-normal normal-case">{ propietario}</span>
             </p>
             <p className="font-bold mb-3 text-gray-700 uppercase"> E-Mail:{''}
                 <span className="font-normal normal-case">{email}</span>
